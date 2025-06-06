618eb6c0b5d017e415ae28ff0cd02cec4b24b3b8
mutiu4luv <chidiemmamadu@gmail.com>
2025-06-06 09:11:09 +0100

    protectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import LandingPage from "./landingPage/LandingPage";
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
 import Members from "./screens/MembersScreen";
-
+import ProtectedRoute from "./components/protectedRoute/ProtectedRoute";
 function App() {
   return (
     <>
@@ -12,7 +12,15 @@ function App() {
         {/* <Route path="/matches" element={<Matches />} />
         <Route path="/messages" element={<Messages />} />
         <Route path="/profile" element={<Profile />} /> */}
-        <Route path="/members/:userId" element={<Members />} />
+        {/* <Route path="/members/:userId" element={<Members />} /> */}
+        <Route
+          path="/members/:userId"
+          element={
+            <ProtectedRoute>
+              <Members />
+            </ProtectedRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Signup />} />
       </Routes>
